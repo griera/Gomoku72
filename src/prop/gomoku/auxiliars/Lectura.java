696cb46eaaa06756bc85720712429e80bc81e5aa
Genis Riera <genis.riera.perez@gmail.com>
2012-11-16 14:06:39 +0100

    Totes les classes del package prop.gomoku.auxiliars estan formatades segons les normes d'estil pactades a nivell de cluster.

--- src/prop/gomoku/auxiliars/Lectura.java
+++ src/prop/gomoku/auxiliars/Lectura.java
@@ -1,19 +1,20 @@
 package prop.gomoku.auxiliars;
 
-public interface Lectura {
-    public char llegirChar();
-    
-    public byte llegirByte();
-    
-    public short llegirShort();
-    
-    public int llegirInt();
-    
-    public long llegirLong();
-    
-    public float llegirFloat();
-    
-    public double llegirDouble();
-    
-    public String llegirString();
+public interface Lectura
+{
+	public char llegirChar();
+
+	public byte llegirByte();
+
+	public short llegirShort();
+
+	public int llegirInt();
+
+	public long llegirLong();
+
+	public float llegirFloat();
+
+	public double llegirDouble();
+
+	public String llegirString();
 }
--- src/prop/gomoku/auxiliars/LecturaBuffers.java
+++ src/prop/gomoku/auxiliars/LecturaBuffers.java
@@ -1,98 +1,134 @@
 package prop.gomoku.auxiliars;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class LecturaBuffers implements Lectura {
-    public char llegirChar() {
-        char c = ' ';
-        try {
-            BufferedInputStream in = new BufferedInputStream(System.in);
-            c = (char) in.read();
-        } catch (IOException e) {
-            System.out.println("Error durant la lectura per teclat");
-            e.printStackTrace(); // Ajuda a debugar errors del programa, imprimint
-                                 // una traça de la pila on es van empilant els
-                                 // mètodes cridat (la pila la gestiona la JVM)
-        }
-        return c;
-    }
-    public byte llegirByte() {
-        String s = null;
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            s = in.readLine();
-        } catch(IOException e) {
-            System.out.println("Error durant la lectura per teclat");
-            e.printStackTrace();
-        }
-        return Byte.parseByte(s);
-    }
-    public short llegirShort() {
-        String s = null;
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            s = in.readLine();
-        } catch(IOException e) {
-            System.out.println("Error durant la lectura per teclat");
-            e.printStackTrace();
-        }
-        return Short.parseShort(s);
-    }
-    public int llegirInt() {
-        String s = null;
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            s = in.readLine();
-        } catch(IOException e) {
-            System.out.println("Error durant la lectura per teclat");
-            e.printStackTrace();
-        }
-        return Integer.parseInt(s);
-    }
-    public long llegirLong() {
-        String s = null;
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            s = in.readLine();
-        } catch(IOException e) {
-            System.out.println("Error durant la lectura per teclat");
-            e.printStackTrace();
-        }
-        return Long.parseLong(s);
-    }
-    public float llegirFloat() {
-        String s = null;
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            s = in.readLine();
-        } catch(IOException e) {
-            System.out.println("Error durant la lectura per teclat");
-            e.printStackTrace();
-        }
-        return Float.parseFloat(s);
-    }
-    public double llegirDouble() {
-        String s = null;
-        try{
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            s = in.readLine();
-        } catch(IOException e) {
-            System.out.println("Error durant la lectura per teclat");
-            e.printStackTrace();
-        }
-        return Double.parseDouble(s);
-    }
-    public String llegirString() {
-        String s = null;
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            s = in.readLine();
-        } catch(IOException e) {
-            System.out.println("Error durant la lectura per teclat");
-            e.printStackTrace();
-        }
-        return s;
-    }
+public class LecturaBuffers implements Lectura
+{
+	public char llegirChar()
+	{
+		char c = ' ';
+		try
+		{
+			BufferedInputStream in = new BufferedInputStream( System.in );
+			c = (char) in.read();
+		} catch ( IOException e )
+		{
+			System.out.println( "Error durant la lectura per teclat" );
+			
+			/*
+			 * Ajuda a debugar errors del programa, imprimint una traça de la pila on es van empilant els mètodes 
+			 * cridats (la pila la gestiona la JVM)
+			 */			
+			e.printStackTrace();
+		}
+		return c;
+	}
+
+	public byte llegirByte()
+	{
+		String s = null;
+		try
+		{
+			BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+			s = in.readLine();
+		} catch ( IOException e )
+		{
+			System.out.println( "Error durant la lectura per teclat" );
+			e.printStackTrace();
+		}
+		return Byte.parseByte( s );
+	}
+
+	public short llegirShort()
+	{
+		String s = null;
+		try
+		{
+			BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+			s = in.readLine();
+		} catch ( IOException e )
+		{
+			System.out.println( "Error durant la lectura per teclat" );
+			e.printStackTrace();
+		}
+		return Short.parseShort( s );
+	}
+
+	public int llegirInt()
+	{
+		String s = null;
+		try
+		{
+			BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+			s = in.readLine();
+		} catch ( IOException e )
+		{
+			System.out.println( "Error durant la lectura per teclat" );
+			e.printStackTrace();
+		}
+		return Integer.parseInt( s );
+	}
+
+	public long llegirLong()
+	{
+		String s = null;
+		try
+		{
+			BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+			s = in.readLine();
+		} catch ( IOException e )
+		{
+			System.out.println( "Error durant la lectura per teclat" );
+			e.printStackTrace();
+		}
+		return Long.parseLong( s );
+	}
+
+	public float llegirFloat()
+	{
+		String s = null;
+		try
+		{
+			BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+			s = in.readLine();
+		} catch ( IOException e )
+		{
+			System.out.println( "Error durant la lectura per teclat" );
+			e.printStackTrace();
+		}
+		return Float.parseFloat( s );
+	}
+
+	public double llegirDouble()
+	{
+		String s = null;
+		try
+		{
+			BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+			s = in.readLine();
+		} catch ( IOException e )
+		{
+			System.out.println( "Error durant la lectura per teclat" );
+			e.printStackTrace();
+		}
+		return Double.parseDouble( s );
+	}
+
+	public String llegirString()
+	{
+		String s = null;
+		try
+		{
+			BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+			s = in.readLine();
+		} catch ( IOException e )
+		{
+			System.out.println( "Error durant la lectura per teclat" );
+			e.printStackTrace();
+		}
+		return s;
+	}
 }
