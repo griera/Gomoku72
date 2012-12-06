8aab4c52c324bd168de8385666f9341338eae166
ignaciocontreras <ignacio@nachovia.com>
2012-12-06 19:33:41 +0100

    Adapta test per ensenyar de forma mes clara els resultats ordenats, imprimint la dada de interes. Encara hi ha algun petit error

--- src/prop/gomoku/tests/TestRecordsGlobals.java
+++ src/prop/gomoku/tests/TestRecordsGlobals.java
@@ -2,22 +2,35 @@ package prop.gomoku.tests;
 
 import java.util.PriorityQueue;
 
-import prop.gomoku.domini.controladors.ControladorRecordsGlobals;
+import prop.gomoku.domini.controladors.records.AccesRecords;
+import prop.gomoku.domini.controladors.records.ControladorRecordsGlobals;
+import prop.gomoku.domini.controladors.records.CriteriRecords;
 import prop.gomoku.domini.models.LlistaRecordsIndividuals;
 
 public class TestRecordsGlobals
 {
-	public static void main(String[] args)
+	public static void main( String[] args )
 	{
-		ControladorRecordsGlobals ctrl_records = new ControladorRecordsGlobals();
-		int pos = 1;
-		PriorityQueue<LlistaRecordsIndividuals> llista = ctrl_records.getRecordsIndividualsOrdenats();
-		LlistaRecordsIndividuals record = llista.poll();
-		while (record != null)
+		for ( CriteriRecords criteri : CriteriRecords.values() )
 		{
-			System.out.println(pos + ". " + record.getUsuari() );
-			pos += 1;
-			record = llista.poll();
+			System.out.println( criteri.toString().toUpperCase() );
+			ControladorRecordsGlobals ctrl_records = new ControladorRecordsGlobals( criteri );
+			int pos = 1;
+			PriorityQueue<LlistaRecordsIndividuals> llista = ctrl_records.getRecordsIndividualsOrdenats();
+			LlistaRecordsIndividuals record = llista.poll();
+			while ( record != null )
+			{
+				int dada_interes = AccesRecords.get( record, criteri );
+				System.out.print( pos + ". " + record.getUsuari().getNom() + ": "+ dada_interes );
+				if (criteri.toString().contains( "PER" ))
+				{
+					System.out.print("%");
+				}
+				System.out.print( " - " + record.getUsuari() );
+				System.out.println();
+				pos += 1;
+				record = llista.poll();
+			}
 		}
 	}
 }
