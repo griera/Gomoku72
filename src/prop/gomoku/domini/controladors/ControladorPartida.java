76cb0dc4f17a6ff9b76e1f3cb16bf8ccc9301997
ignaciocontreras <ignacio@nachovia.com>
2012-12-08 12:17:30 +0100

    Elimina duplicacio de responsabilitats. No es creen partides segons tipus de jugador, ja que el controlador de usuari permet carregar el usuari que pertoqui, menys metodes, mes simple

--- src/prop/gomoku/domini/controladors/ControladorPartida.java
+++ src/prop/gomoku/domini/controladors/ControladorPartida.java
@@ -1,12 +1,8 @@
 package prop.gomoku.domini.controladors;
 
-import java.util.List;
-
-//import prop.gomoku.domini.models.TipusUsuari;
 import prop.gomoku.domini.models.PartidaGomoku;
 import prop.gomoku.domini.models.TaulerGomoku;
 import prop.gomoku.domini.models.UsuariGomoku;
-import prop.gomoku.gestors.GestorPartidesGuardades;
 
 public class ControladorPartida
 {
@@ -28,51 +24,4 @@ public class ControladorPartida
 		return partida;
 	}
 
-	// TODO potser aquestes no cal fer-les, ja que complicaran la gestió de les partides. Si des de CtlrUsuari ja podem
-	// carregar-ne els de sistema, no crec que faci falta fer servir un altre mètode que no sigui el de creació
-	// estàndard de partida
-	/**
-	 * Mètode que permet la creació d'una `p
-	 * 
-	 * @param jugador_principal
-	 * @param jugador_negres
-	 * @param ia_blanques
-	 * @param nom_partida
-	 * @return
-	 */
-//	public PartidaGomoku creaNovaPartida( UsuariGomoku jugador_principal, UsuariGomoku jugador_negres,
-//			TipusUsuari ia_blanques, String nom_partida )
-//	{
-//		// TODO
-//		return null;
-//	}
-//
-//	public PartidaGomoku creaNovaPartida( UsuariGomoku jugador_principal, TipusUsuari ia_negres,
-//			UsuariGomoku jugador_blanques, String nom_partida )
-//	{
-//		// TODO
-//		return null;
-//	}
-//
-//	public PartidaGomoku creaNovaPartida( UsuariGomoku jugador_principal, TipusUsuari ia_negres,
-//			TipusUsuari ia_blanques, String nom_partida )
-//	{
-//		// TODO
-//		return null;
-//	}
-
-	// TODO duplica funcionalitat ja existent
-	public List<PartidaGomoku> carregaPartides( UsuariGomoku usuari )
-	{
-		GestorPartidesGuardades gestor = new GestorPartidesGuardades();
-		return gestor.carregaPartides( usuari );
-	}
-
-	// TODO duplica funcionalitat ja existent
-	public String guardaPartida( PartidaGomoku partida )
-	{
-		GestorPartidesGuardades gestor = new GestorPartidesGuardades();
-		// Presuposa que partida conté informació a sobre de l'amo d'aquesta
-		return gestor.guardaPartida( partida );
-	}
 }
