2e8796d4e06a0989a6abc7fef22ab19af5963bf8
ignaciocontreras <ignacio@nachovia.com>
2012-12-08 15:23:15 +0100

    Actualitza documentacio InteligenciaCPU

--- src/prop/gomoku/domini/controladors/InteligenciaCPU.java
+++ src/prop/gomoku/domini/controladors/InteligenciaCPU.java
@@ -5,13 +5,28 @@ import prop.gomoku.domini.models.PartidaGomoku;
 import prop.gomoku.domini.models.TaulerGomoku;
 import prop.gomoku.domini.models.TipusUsuari;
 
+/**
+ * Classe que permet abstraure les diferents possibilitats de inteligències artificials al sistema. Cada instància
+ * d'aquesta classe té associada un nivell de CPU i el paper que juguen a la partida.
+ * 
+ * @author Mauricio Ignacio Contreras Pinilla
+ * 
+ */
 public class InteligenciaCPU
 {
 	private TipusUsuari nivell;
 	private EstatCasella color;
 	private IAGomoku ia;
 
-	public InteligenciaCPU( TipusUsuari nivell, EstatCasella color )
+	/**
+	 * Mètode constructor
+	 * 
+	 * @param nivell Nivell de dificultat de la CPU que s'associa
+	 * @param color Paper amb el que ha la IA hauria de jugar la partida
+	 * @throws IllegalArgumentException Si el <em>nivell</em> no identifica a una dificultat de CPU o si <em>color</em>
+	 *         és <em>EstatCasella.BUIDA</em>
+	 */
+	public InteligenciaCPU( TipusUsuari nivell, EstatCasella color ) throws IllegalArgumentException
 	{
 		if ( nivell == TipusUsuari.HUMA || nivell == TipusUsuari.CONVIDAT )
 		{
@@ -25,7 +40,7 @@ public class InteligenciaCPU
 
 		this.nivell = nivell;
 		this.color = color;
-		
+
 		switch ( nivell )
 		{
 			case FACIL:
@@ -44,7 +59,18 @@ public class InteligenciaCPU
 		}
 	}
 
-	// TODO documentar
+	/**
+	 * Donada una partida específica i l'últim moviment realitzat, aquest mètode permet computar el moviment de la IA
+	 * segons el nivell de dificultat del paràmetre implícit
+	 * 
+	 * Les coordenades de l'últim moviment d'interès (<em>fila</em>, <em>columna</em>), és informació extra que pot
+	 * interessar a les diferents implementacions de IA
+	 * 
+	 * @param partida Partida a la qual es vol realitzar un moviment
+	 * @param fila_ult_moviment Fila de l'últim moviment d'interès
+	 * @param columna_ult_moviment Columna de l'últim moviment d'interès
+	 * @return
+	 */
 	public int[] getMoviment( PartidaGomoku partida, int fila_ult_moviment, int columna_ult_moviment )
 	{
 		int[] moviment_ia = new int[2];
@@ -76,7 +102,11 @@ public class InteligenciaCPU
 		return moviment_ia;
 	}
 
-	// TODO documentar
+	/**
+	 * Mètode consultor del nivell de dificultat associat al paràmetre implícit
+	 * 
+	 * @return Nivell de la CPU amb el que aquesta InteligenciaCPU juga
+	 */
 	public TipusUsuari getNivellCPU()
 	{
 		return this.nivell;
