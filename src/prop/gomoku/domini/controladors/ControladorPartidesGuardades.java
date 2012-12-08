3b6432fba3309afec3256c37412f48abb9fc7797
ignaciocontreras <ignacio@nachovia.com>
2012-12-08 12:34:25 +0100

    Documentacio de ControladorPartidesGuardades al dia

--- src/prop/gomoku/domini/controladors/ControladorPartidesGuardades.java
+++ src/prop/gomoku/domini/controladors/ControladorPartidesGuardades.java
@@ -6,19 +6,38 @@ import prop.gomoku.domini.models.PartidaGomoku;
 import prop.gomoku.domini.models.UsuariGomoku;
 import prop.gomoku.gestors.GestorPartidesGuardades;
 
+/**
+ * Controlador que permet guardar i carregar partides
+ * 
+ * @author Mauricio Ignacio Contreras Pinilla
+ * 
+ */
 public class ControladorPartidesGuardades
 {
-	GestorPartidesGuardades gestor = new GestorPartidesGuardades();
+	/**
+	 * Gestor de partides guardades encarregat de gestionar la interacció amb disc
+	 */
+	private GestorPartidesGuardades gestor = new GestorPartidesGuardades();
 
+	/**
+	 * Mètode per carregar totes les partides d'un usuari
+	 * 
+	 * @param usuari Usuari al qual pertanyen les partides que es volen carregar
+	 * @return Llista de les partides que pertanyen al usuari indicat
+	 */
 	public List<PartidaGomoku> carregaPartides( UsuariGomoku usuari )
 	{
-		// TODO
 		return this.gestor.carregaPartides( usuari );
 	}
 
+	/**
+	 * Mètode per guardar una partida específica
+	 * 
+	 * @param partida Partida que es es vol guardar
+	 * @return <em>true</em> si s'ha guardat la partida amb èxit; <em>false</em> en cas contrari
+	 */
 	public boolean guardaPartida( PartidaGomoku partida )
 	{
-		// TODO
 		if ( this.gestor.guardaPartida( partida ) != null )
 		{
 			return true;
