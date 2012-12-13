b0b41ad414271b2226f9e8960c7984175760c283
ignaciocontreras <ignacio@nachovia.com>
2012-12-13 19:56:53 +0100

    Primera versio ControladorRecordsIndividuals

--- src/prop/gomoku/domini/controladors/records/ControladorRecordsIndividuals.java
+++ src/prop/gomoku/domini/controladors/records/ControladorRecordsIndividuals.java
@@ -1,6 +1,20 @@
 package prop.gomoku.domini.controladors.records;
 
+import prop.gomoku.domini.models.LlistaRecordsIndividuals;
+import prop.gomoku.domini.models.UsuariGomoku;
+
 public class ControladorRecordsIndividuals
 {
+	// TODO
+	private LlistaRecordsIndividuals records;
 
+	public ControladorRecordsIndividuals( UsuariGomoku usuari )
+	{
+		records = new LlistaRecordsIndividuals( usuari );
+	}
+
+	public int getLlistaRecords( CriteriRecords criteri )
+	{
+		return AccesRecords.get( records, criteri );
+	}
 }
